811a2a4df726174514088c7d632cb5b76836bef7
fanboimsft <fanboimsft@icloud.com>
2024-03-14 17:53:54 +0000

    fix

--- files/flags.sh
+++ files/flags.sh
@@ -1,15 +1,11 @@
 export KISS_ROOT=$rootfsdir
 export KISS_TMPDIR=/tmp
-export KISS_PROMPT=0
 export KISS_SU=su
-export KISS_COMPRESS=xz
-export KISS_HOOK=$filesdir/kiss-hook
-export KISS_PATH=/repo/sauzeros/core:/repo/sauzeros/extra:/repo/sauzeros/xorg
-export KISS_PATH=$KISS_PATH:/repo/kiss/core:/repo/kiss/extra:/repo/kiss/wayland
-export KISS_PATH=$KISS_PATH:/repo/community/community
+export KISS_COMPRESS=zst
+export KISS_PATH=/repo/sauzeros/core:/repo/sauzeros/extra:/repo/sauzeros/xorg:/repo/sauzeros/python
 export CFLAGS="-march=x86-64 -mtune=generic -pipe -O2"
 export CXXFLAGS="$CFLAGS"
 export MAKEFLAGS="-j$(($(nproc)+2))"
-export XZ_OPT="-T0"
+export XZ_OPT="-T0 -M 90%"
 
 
